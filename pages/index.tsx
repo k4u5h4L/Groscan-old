65c104ce3308809a5e50324910e24e21498c171a
k4u5h4L <kaushal.v.bhat@gmail.com>
2022-08-06 13:59:18 +0530

    set up assets and install deps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,7 @@
-import { BarcodeScanner } from "@capacitor-community/barcode-scanner";
-import { useEffect } from "react";
-
 export default function Home() {
-    const startScan = async () => {
-        const status = await BarcodeScanner.checkPermission({ force: true });
-
-        if (status) {
-            await BarcodeScanner.hideBackground(); // make background of WebView transparent
-
-            // if (document) {
-            //     document.body.requestFullscreen();
-            // }
-            // start scanning and wait for a result
-            const result = await BarcodeScanner.startScan();
-
-            // if the result has content
-            if (result.hasContent) {
-                alert(result.content); // log the raw scanned content
-            }
-
-            await BarcodeScanner.stopScan();
-            await BarcodeScanner.showBackground();
-        }
-    };
-
-    useEffect(() => {
-        startScan();
-    });
-
     return (
         <>
-            <div className="sample-background">
-                {/* Scanner will come here */}
-            </div>
-            <div className="container">
-                <div className="barcode-scanner--area--container">
-                    <div className="relative">
-                        <p>Aim your camera at a barcode</p>
-                    </div>
-                    <div className="square surround-cover">
-                        <div className="barcode-scanner--area--outer surround-cover">
-                            <div className="barcode-scanner--area--inner"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <h1>Hello</h1>
         </>
     );
 }
